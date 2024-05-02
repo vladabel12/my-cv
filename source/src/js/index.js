7fe70e507447b63a1e642f0084d66d226fc6a909
vladabel12 <vladabel12@gmail.com>
2024-05-02 17:41:55 +0300

    added mixins

--- source/src/js/index.js
+++ source/src/js/index.js
@@ -1,2 +1,5 @@
 // import header from './modules/header.js';
 // header();
+document.getElementById('downloadButton').addEventListener('click', function() {
+    window.location.href = './Hello.pdf';
+});
